3743b86b3009415d7242b5f89717168983f766e0
James Draper <jd.unchecked@gmail.com>
2022-08-13 19:13:03 +0100

    Renamed Concatenate to ConcatenatePattern.

--- src/Pattern/ConcatenatePattern.php
+++ src/Pattern/ConcatenatePattern.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace LuaT\Pattern;
 
-class Concatenate extends SimplePattern
+class ConcatenatePattern extends SimplePattern
 {
     public function __construct()
     {
--- test/Pattern/ConcatenatePatternTest.php
+++ test/Pattern/ConcatenatePatternTest.php
@@ -5,20 +5,20 @@ namespace Test\Pattern;
 
 use PHPUnit\Framework\TestCase;
 
-use LuaT\Pattern\Concatenate;
+use LuaT\Pattern\ConcatenatePattern;
 
 use function preg_match;
 
-class ConcatenateTest extends TestCase
+class ConcatenatePatternTest extends TestCase
 {
-    private Concatenate $concatenate;
+    private ConcatenatePattern $concatenatePattern;
 
     /**
      * @test
      */
     public function it_should_match_concatenate_sequences(): void
     {
-        preg_match('~' . $this->concatenate . '~', 'a..a', $matches);
+        preg_match('~' . $this->concatenatePattern . '~', 'a..a', $matches);
 
         $this->assertCount(1, $matches);
         $this->assertSame('..', $matches[0]);
@@ -29,7 +29,7 @@ class ConcatenateTest extends TestCase
      */
     public function it_should_not_match_non_concatenate_sequences(): void
     {
-        preg_match('~' . $this->concatenate . '~', 'aa', $matches);
+        preg_match('~' . $this->concatenatePattern . '~', 'aa', $matches);
 
         $this->assertCount(0, $matches);
     }
@@ -38,6 +38,6 @@ class ConcatenateTest extends TestCase
     {
         parent::setUp();
 
-        $this->concatenate = new Concatenate;
+        $this->concatenatePattern = new ConcatenatePattern;
     }
 }
