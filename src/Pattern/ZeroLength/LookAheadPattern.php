45da86fb512fe6e3f74b7abbeeeaf7a9b35eeb2e
James Draper <jd.unchecked@gmail.com>
2022-09-04 10:12:57 +0100

    Add zero length assertions.

---
+++ src/Pattern/ZeroLength/LookAheadPattern.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace LuaT\Pattern\ZeroLength;
+
+use LuaT\Pattern\SimplePattern;
+use LuaT\Pattern\Pattern;
+
+use function sprintf;
+
+class LookAheadPattern extends SimplePattern
+{
+    public function __construct(string|Pattern $pattern)
+    {
+        $pattern = (string) sprintf('(?=%s)', $pattern);
+
+        parent::__construct($pattern);
+    }
+}
