8f1aa8120728201a495b0ad2c91fc6f8289738d1
James Draper <jd.unchecked@gmail.com>
2022-10-02 15:30:45 +0100

    removed redundant tests

--- test/Pattern/LuaSyntax/EqualsPatternTest.php
+++ test/Pattern/LuaSyntax/EqualsPatternTest.php
@@ -9,36 +9,6 @@ use Test\Pattern\PatternTestCase;
 
 class EqualsPatternTest extends PatternTestCase
 {
-    private EqualsPattern $equalsPattern;
-
-    /**
-     * @test
-     */
-    public function it_should_match_concatenate_sequences(): void
-    {
-        preg_match('~' . $this->equalsPattern . '~', 'a=a', $matches);
-
-        $this->assertCount(1, $matches);
-        $this->assertSame('=', $matches[0]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_should_not_match_non_concatenate_sequences(): void
-    {
-        preg_match('~' . $this->equalsPattern . '~', 'aa', $matches);
-
-        $this->assertCount(0, $matches);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->equalsPattern = new EqualsPattern;
-    }
-
     public function getPatternClass(): string
     {
         return EqualsPattern::class;
